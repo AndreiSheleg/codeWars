6bb2a360a1689b8c1b4f21a5713d9a3a965c3dbe
Andrei <106683554+AndreiSheleg@users.noreply.github.com>
2022-10-30 09:19:35 +0300

    Update 17 Fake Binary.js
    Fixed logic errors

--- 17 Fake Binary.js
+++ 17 Fake Binary.js
@@ -1,14 +1,13 @@
 function fakeBin(x){
-let result = "";
-let newArr = x.split();
+let result = [];
+let newArr = x.split("");
 
   for (let i = 0; i < newArr.length; i++ ) {
     if (newArr[i] < 5) {
-      newArr[i] = 0;
-    } else if (newArr[i] => 5) {
-      newArr[i] = 1;
+      result.push(0);
+    } else {
+      result.push(1);
     };
-    result = newArr.join('');
-  };
-  return result;
+    };
+  return result.join("");
 }
