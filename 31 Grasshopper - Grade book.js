23c07009bdb7b506c56e6e92d27c0e85220fc11e
Andrei Sheleg <andrej.sheleg@gmail.com>
2022-11-09 10:13:40 +0300

    Create 31 Grasshopper - Grade book.js
    Grade book
    Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
    
    Numerical Score	Letter Grade
    90 <= score <= 100	'A'
    80 <= score < 90	'B'
    70 <= score < 80	'C'
    60 <= score < 70	'D'
    0 <= score < 60	'F'
    Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.

---
+++ 31 Grasshopper - Grade book.js
@@ -0,0 +1,16 @@
+function getGrade (s1, s2, s3) {
+  // Code here
+  let result = (s1 + s2 + s3) / 3;
+  
+  if (result >= 90 ) {
+    return "A";
+  } else if (result >= 80 ) {
+    return "B";
+  } else if (result >= 70 ) {
+    return "C";
+  } else if (result >= 60 ) {
+    return "D";
+  } else {
+    return  "F";
+    };
+};
