825321767571d8c8b243fde9b75236bd0f79981c
Andrei Sheleg <andrej.sheleg@gmail.com>
2022-11-17 13:04:35 +0300

    Create 48  Keep up the hoop.js
    Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
    
    Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
    
    If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
    If he doesn't get 10 hoops, return the string "Keep at it until you get it".

---
+++ 48  Keep up the hoop.js
@@ -0,0 +1,8 @@
+function hoopCount (n) {
+   //your code goes here    
+  let message = "Keep at it until you get it";
+  if (n >= 10) {
+    message = "Great, now move on to tricks";
+  };
+  return message;
+}
