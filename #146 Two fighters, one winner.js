4027c8900bd8e41e45beb129bff002d2178e68f5
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-01-14 10:39:10 +0300

    Create #146 Two fighters, one winner.js
    Create a function that returns the name of the winner in a fight between two fighters.
    
    Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
    
    Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
    
    Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
    
    Your function also receives a third argument, a string, with the name of the fighter that attacks first.
    
    Example:
      declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
      
      Lew attacks Harry; Harry now has 3 health.
      Harry attacks Lew; Lew now has 6 health.
      Lew attacks Harry; Harry now has 1 health.
      Harry attacks Lew; Lew now has 2 health.
      Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
    function Fighter(name, health, damagePerAttack) {
            this.name = name;
            this.health = health;
            this.damagePerAttack = damagePerAttack;
            this.toString = function() { return this.name; }
    }

---
+++ #146 Two fighters, one winner.js
@@ -0,0 +1,5 @@
+const declareWinner = (fighter1, fighter2, firstAttacker) => {
+  let f1 = Math.ceil(fighter1.health / fighter2.damagePerAttack)
+  let f2 = Math.ceil(fighter2.health / fighter1.damagePerAttack)
+  return f1 < f2 ? fighter2.name : f1 > f2 ? fighter1.name : firstAttacker
+}
