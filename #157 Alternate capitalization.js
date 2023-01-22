8f8bd48c09b4659e53b400d29c2dc077d513c1f7
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-01-22 16:33:12 +0300

    Update #157 Alternate capitalization.js
    add commit about even || odd

--- #157 Alternate capitalization.js
+++ #157 Alternate capitalization.js
@@ -1,8 +1,8 @@
 const capitalize = s => {
   
-  //проверка чётность и нечётность
-  let word1 = s.split('').map( (v,i) => i % 2 === 0 ? v.toUpperCase() : v.toLowerCase() ).join('')
-  let word2 = s.split('').map( (v,i) => i % 2 !== 0 ? v.toUpperCase() : v.toLowerCase() ).join('')
+  //проверка чётность и нечётность even || odd
+  let word1 = s.split('').map( (x,i) => i % 2 === 0 ? x.toUpperCase() : x.toLowerCase() ).join('')
+  let word2 = s.split('').map( (x,i) => i % 2 !== 0 ? x.toUpperCase() : x.toLowerCase() ).join('')
    
   return [word1, word2];
 };
