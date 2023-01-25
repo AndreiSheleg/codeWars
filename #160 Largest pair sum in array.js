b4eab73b609d16727c750d3840b9341f5061246e
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-01-26 01:52:05 +0300

    Update #160 Largest pair sum in array.js

--- #160 Largest pair sum in array.js
+++ #160 Largest pair sum in array.js
@@ -1,1 +1,3 @@
 const largestPairSum = numbers => numbers.sort( (a, b) => a - b ).slice(-2).reduce( (a,b) => a + b, 0)
+
+// const largestPairSum = numbers => numbers.sort((a, b) => b - a).slice(0, 2).reduce((a, b) => a + b);
