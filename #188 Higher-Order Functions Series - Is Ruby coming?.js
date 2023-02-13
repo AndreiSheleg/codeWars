8ee1e8ab52769cbb55a230e624f33e131871a0f7
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-02-13 14:15:51 +0300

    Update #188 Higher-Order Functions Series - Is Ruby coming?.js
    add a shorter solutions

--- #188 Higher-Order Functions Series - Is Ruby coming?.js
+++ #188 Higher-Order Functions Series - Is Ruby coming?.js
@@ -1,1 +1,3 @@
 const isRubyComing = list => list.filter(el => el.language === 'Ruby').length !== 0 ? true : false
+
+// const isRubyComing = list => list.filter(el => el.language === 'Ruby').length !== 0
