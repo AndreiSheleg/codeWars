63381f162671730c23a16faf41ac4e2d45d24540
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-02-20 01:21:32 +0300

    Update #195 SevenAte9.js
    delete console.log

--- #195 SevenAte9.js
+++ #195 SevenAte9.js
@@ -1,7 +1,6 @@
 const sevenAte9 = str => {
   let resultStr = str[0]
-  console.log(resultStr)
-  
+    
   for (let i = 1; i < str.length - 1; i++) {
     resultStr += (str[i - 1] === '7' && str[i] === '9' && str[i + 1] === '7') ? '' : str[i]
   }
