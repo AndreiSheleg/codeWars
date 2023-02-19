107d200ef6afc7a3ff9a5baed0d0dedae63efa97
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-02-20 01:20:57 +0300

    Create #195 SevenAte9.js
    Write a function that removes every lone 9 that is inbetween 7s.
    
    "79712312" --> "7712312"
    "79797"    --> "777"

---
+++ #195 SevenAte9.js
@@ -0,0 +1,12 @@
+const sevenAte9 = str => {
+  let resultStr = str[0]
+  console.log(resultStr)
+  
+  for (let i = 1; i < str.length - 1; i++) {
+    resultStr += (str[i - 1] === '7' && str[i] === '9' && str[i + 1] === '7') ? '' : str[i]
+  }
+  
+  resultStr += str[str.length - 1]
+  
+  return resultStr 
+}
