1e7aea794dc531971ee47863ebd697daf1e754f3
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-02-24 00:34:08 +0300

    Update #201 Multiples of 3 or 5.js
    fixed logical

--- #201 Multiples of 3 or 5.js
+++ #201 Multiples of 3 or 5.js
@@ -1,24 +1,11 @@
 const solution = number => {
-  let arr = []
-  let multiple = []
-  
-  // push all num between 0 and the number to arr
-  for (let i = 0; i < number; i++) {
-    arr.push(i)
-  }
-  
-  // if number in arr is a multiple or 3 or 5 push it to multiple
-  for (let i = 0; i < arr.length; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      multiple.push(i)
+let sumTotal = 0
+
+  for(let i = 1; i < number; i++) {
+    if(i % 3 === 0 || i % 5 === 0) {
+      sumTotal += i
     }
   }
   
-  // sum the total of the multiple array
-  let sumTotal = 0
-  for (let i = 0; i < multiple.length; i++) {
-    sumTotal += multiple[i]
-  }
-  
-  return sumTotal;
+  return sumTotal
 }
