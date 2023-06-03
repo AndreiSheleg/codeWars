69065f36ac2729e2b19f650d53e1abe4c5b166ee
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-06-03 08:51:37 +0300

    Update #273  Longest vowel chain.js
    The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.

--- #273  Longest vowel chain.js
+++ #273  Longest vowel chain.js
@@ -1,28 +1,32 @@
 const solve = s => {
   let result = 1
+  console.log('--> текущее s: ', s)
   
   for (let i = 0; i < s.length - 1; i++) {
-    let senten1 = s[i] === 'a' ? 
+    let count = 1
+    let newArr = []
+    console.log('текущий i: ', i)
+    let condition1 = s[i] === 'a' ? 
     true : s[i] === 'e' ?
     true : s[i] === 'i' ?
     true : s[i] === 'o' ?
     true : s[i] === 'u' ?
-    false : false
+    true : false
       
-    console.log('s[i] = ', s[i], ' ; senten1 = ', senten1)
+    console.log('s[i] = ', s[i], ' ; condition1 = ', condition1)
     
-    let senten2 = s[i + 1] === 'a' ? 
+    let condition2 = s[i + 1] === 'a' ? 
     true : s[i + 1] === 'e' ?
     true : s[i + 1] === 'i' ?
     true : s[i + 1] === 'o' ?
     true : s[i + 1] === 'u' ?
-    false : false
+    true : false
     
-    console.log('s[i+1] = ', s[i+1], ' ; senten2 = ', senten2)
+    console.log('s[i+1] = ', s[i+1], ' ; condition2 = ', condition2)
     
-    if( senten1 && senten2) {
+    if( condition1 && condition2 ) {
       result += 1
-      console.log('result = ', result)
+      console.log('---------------------result = ', result)
     }
   }
   
