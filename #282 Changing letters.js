001d60bc902e364e931e83f1c239e561c7a44646
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-06-14 22:24:54 +0300

    Create #282 Changing letters.js
    When provided with a String, capitalize all vowels
    
    For example:
    
    Input : "Hello World!"
    
    Output : "HEllO WOrld!"
    
    Note: Y is not a vowel in this kata.

---
+++ #282 Changing letters.js
@@ -0,0 +1,17 @@
+const swap = string => {
+  let result = []
+  let arrString = string.split('')
+  console.log('--> arrString: ', arrString)
+  if(!string) return ''
+  
+  for (let i = 0; i < string.length; i++) {
+    let senten1 = arrString[i] === 'a' ? 
+    true : s[i] === 'e' ?
+    true : s[i] === 'i' ?
+    true : s[i] === 'o' ?
+    true : s[i] === 'u' ?
+    false : false
+    true : false
+    
+  }
+}
