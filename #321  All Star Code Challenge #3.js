49dcb0d39f06ddd79022d5ccb2ddc242f695fc98
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-08-08 13:13:16 +0300

    Update #321  All Star Code Challenge #3.js

--- #321  All Star Code Challenge #3.js
+++ #321  All Star Code Challenge #3.js
@@ -1,1 +1,6 @@
 const removeVowels = str => str.replace(/[aeiou]/gi, '')
+
+
+// const removeVowels = str => {
+// const vowels = ['a', 'e', 'i', 'o', 'u']
+// return str.split('').filter(el => !vowels.includes(el) ).join('')
