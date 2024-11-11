ac1747e9b2e064a93cc5d3f47465daa87cf2a59a
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-11-11 12:52:35 +0300

    Create #448 ToLeetSpeak.js
    Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
    
    More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
    
    Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
    
    For example:
    
    toLeetSpeak("LEET") returns "1337"
    In this kata we use a simple LeetSpeak dialect. Use this alphabet:
    
    {
      A : '@',
      B : '8',
      C : '(',
      D : 'D',
      E : '3',
      F : 'F',
      G : '6',
      H : '#',
      I : '!',
      J : 'J',
      K : 'K',
      L : '1',
      M : 'M',
      N : 'N',
      O : '0',
      P : 'P',
      Q : 'Q',
      R : 'R',
      S : '$',
      T : '7',
      U : 'U',
      V : 'V',
      W : 'W',
      X : 'X',
      Y : 'Y',
      Z : '2'
    }

---
+++ #448 ToLeetSpeak.js
@@ -0,0 +1,18 @@
+const toLeetSpeak = str => {
+  const leetAlphabet = {
+  A : '@',  B : '8',  C : '(',  D : 'D',  E : '3',  F : 'F',  G : '6',  H : '#',  I : '!',  J : 'J',
+  K : 'K',  L : '1',  M : 'M',  N : 'N',  O : '0',  P : 'P',  Q : 'Q',  R : 'R',  S : '$',  T : '7',
+  U : 'U',  V : 'V',  W : 'W',  X : 'X',  Y : 'Y',  Z : '2'
+}
+  let result = ''
+  
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i]
+    result += leetAlphabet[currentChar] || currentChar
+  }
+  
+  return result
+//   let worfsArr = str.split(' ')
+//   console.log('worfsArr: ', worfsArr)
+
+}
