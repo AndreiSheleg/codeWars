5b9ccc0249be05d88c85a9a1542a1d4739d67520
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-11-19 22:41:53 +0300

    Update #453 Simple Fun #2: Circle of Numbers.js
    Add other solution

--- #453 Simple Fun #2: Circle of Numbers.js
+++ #453 Simple Fun #2: Circle of Numbers.js
@@ -4,3 +4,17 @@ const circleOfNumbers = (n, firstNumber) => {
     
     return oppositePosition;
 }
+
+// const circleOfNumbers = (n, firstNumber) => {  
+//     if (n < 4 || n > 1000) {
+//       console.log('n is invalid');
+//     }
+    
+//     if ( firstNumber < 0 || firstNumber > (n - 1) ) {
+//       console.log('firstNumber is invalid')
+//     }
+        
+//     const half = ( n / 2 )
+    //     return ( firstNumber < half ? ( half + firstNumber ) : ( firstNumber - half ))
+    
+// }
