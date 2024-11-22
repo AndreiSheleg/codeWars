31ff188eb0012c3b49e846af5affe0924f8cc785
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-11-22 17:37:04 +0300

    Update #456 Character Counter.js
    add other solution

--- #456 Character Counter.js
+++ #456 Character Counter.js
@@ -2,8 +2,7 @@ const validateWord = s => {
 
     let sLowerCase = s.toLowerCase()
 
-    let charCount = {};
-
+    let charCount = {}
 
     // Count occurrences of each character in the input string
     [...sLowerCase].forEach(char => {
@@ -12,5 +11,17 @@ const validateWord = s => {
 
     let counts = Object.values(charCount)
 
-    return counts.every(count => count === counts[0]);
+    return counts.every(count => count === counts[0])
 }
+
+//const validateWord = s => {
+//   s = s.toLowerCase().split("")
+//   let arr = []
+
+//   for (let i = 0; i < s.length; i++) {
+//     let current = s.filter(el => el === el[i]).length
+//     if(arr.includes(current)) continue; // Если условие if истинно (то есть элемент уже есть в массиве), оператор continue прерывает текущую итерацию цикла и переходит к следующей итерации  
+//        arr.push(current);
+//   }
+//   return arr.length === 1
+// }
