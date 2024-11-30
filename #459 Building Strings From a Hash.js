a35f78934f6930258aa8de0ff259cb110965ef8b
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-11-30 22:25:18 +0300

    Update #459 Building Strings From a Hash.js
    add other solution

--- #459 Building Strings From a Hash.js
+++ #459 Building Strings From a Hash.js
@@ -3,3 +3,15 @@ const solution = pairs => {
         .map(([key, value]) => `${key} = ${value}`)
         .join(',')
 }
+
+// const solution = pairs => { 
+//     let array = [] 
+//     let keys = Object.keys(pairs)
+
+//     for (let i = 0; i < keys.length; i++) { 
+//         let pair = keys[i] 
+//         array.push( (pair + ' = ' + pairs[pair]) )
+//     } 
+
+//     return array.join(',') 
+// }
