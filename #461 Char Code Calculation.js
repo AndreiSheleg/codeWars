65b642a98d594dc7b72fa37100668625cfac9506
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-12-03 18:50:54 +0300

    Update #461 Char Code Calculation.js
    add other solution

--- #461 Char Code Calculation.js
+++ #461 Char Code Calculation.js
@@ -10,3 +10,13 @@ const calc = x => {
     
     return sumTotal1 - sumTotal2
 }
+
+// const calc = x => {
+
+//   let sum = n => [...n].reduce((a,b) => parseInt(a) + parseInt(b) )
+
+//   let total1 = x.replace(/./g, x => x.charCodeAt(0))
+//   let total2 = total1.replace(/7/g,'1')
+
+//   return sum(total1) - sum(total2)
+// }
