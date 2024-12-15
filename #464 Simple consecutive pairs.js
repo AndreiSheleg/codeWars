f63efcc554d5bccf6ee8a62b03b4c2b933fb403f
Andrei Sheleg <andrej.sheleg@gmail.com>
2024-12-16 00:00:17 +0300

    Update #464 Simple consecutive pairs.js

--- #464 Simple consecutive pairs.js
+++ #464 Simple consecutive pairs.js
@@ -9,3 +9,11 @@ conat pairs = ar => {
 
     return count
 }
+
+// function pairs(arr){
+//   let count = 0
+//   for (let i=0; i < arr.length;  i+= 2)
+//     if (arr[i] - 1 == arr[i + 1] || arr[i] + 1 == arr[i + 1])
+//       count++;
+//   return count;
+// };
